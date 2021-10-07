bf2d1d75d5471d570fea5578eea12ec9b9c931cf
GILLYBOEUF <marie-alix.gillyboeuf@epfl.ch>
2021-10-07 17:36:38 +0200

    add mse to ridge

--- Project 1/scripts/helpers.py
+++ Project 1/scripts/helpers.py
@@ -4,15 +4,15 @@ def least_squares(y, tx):
     """calculate the least squares solution."""
     a = tx.T.dot(tx)
     b = tx.T.dot(y)
-    opt_w = np.linalg.solve(a, b)
-    e = y - tx.dot(opt_w)
-    mse = 1/2*np.mean(e**2)
+    w = np.linalg.solve(a, b)
+    mse = compute_mse (y, tx, w)
     # returns mse, and optimal weights
-    return mse, opt_w
+    return mse, weights
 
 def ridge_regression(y, tx, lambda_):
     """implement ridge regression."""
     a = tx.T.dot(tx) + 2*tx.shape[0]*lambda_*np.identity(tx.shape[1])
     b = tx.T.dot(y)
-    weights = np.linalg.solve(a, b)
-    return weights
+    w = np.linalg.solve(a, b)
+    mse = compute_mse (y, tx, w)
+    return mse, w

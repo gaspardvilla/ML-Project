5864a54e81ae866850bdb1b012c8f5e9c69863af
GILLYBOEUF <marie-alix.gillyboeuf@epfl.ch>
2021-10-19 17:49:48 +0200

    function EDA, median instead of mean, classification, implementations

--- Project 1/scripts/EDA.py
+++ Project 1/scripts/EDA.py
@@ -23,27 +23,101 @@ def train_test_separator(truth, large_set, spliter, seed):
     # Return the results
     return train_set, train_truth, test_set, test_truth
 
-
+# -------------------------------------------------------------------------- #
 
 def standardize(data_set):
     data_set = data_set - np.mean(data_set, axis=0)
     data_set = data_set / np.std(data_set, axis=0)
     return data_set
 
+# -------------------------------------------------------------------------- #
 
+def constant_feature (feature):
+    constant = feature[0]
+    for i in range (len(feature) - 1):
+        if (feature[i] != constant):
+            return False
+    return True
 
-def categorization(data_set):
+# -------------------------------------------------------------------------- #
+
+def clean_constant_features(tx):
+    ind_const = []
+    for i in range (tx.shape[1]):
+        if (constant_feature(tx[:,i]) == True):
+            ind_const.append(i)
+            print('constant feature : ' + str(i + 1))      
+    tx = np.delete(tx, ind_const, axis = 1)
+    return tx
+
+# -------------------------------------------------------------------------- #
+
+def clean_correlated_features(data_set):
+    '''correlation matrix'''
+    C = np.corrcoef(data_set.T) # correlation coefficient between features
+                          # C is a 30x30 array
+                          # Cij is the correlation coefficient between feature i and feature j 
+    # Select upper triangle of correlation matrix
+    C = np.triu(C, k=0)
+
+    '''get rid of features with correlation coefficient > 0.95'''
+    threshold = 0.95
+    corr_features = []
+    for i in range(len(C[0]) - 1):
+        for j in range(len(C[0]) - 1):
+            if ((i != j) and C[i][j] > threshold):
+                corr_features.append([i, j])
+    # if there is no correlated features, return the data set
+    if (not corr_features):
+        return data_set
+    print('correlation')
+    corr_feat_to_delete = [corr_features[0][0]]
+    for i in range (1, len(corr_features) - 1):
+        if (corr_features[i][0] != corr_features[i-1][0]):
+            corr_feat_to_delete.append(corr_features[i][0])
+
+    data_set= np.delete(data_set, corr_feat_to_delete, axis = 1) 
+    return data_set
+
+# -------------------------------------------------------------------------- #
+
+def indices_classification(data_set):
+    # Put the data in one of the four classes in function of the value of feature 23
     # Direct computation
-    cat_A = np.array(np.where(data_set[:,22] == 0)[0])
-    cat_B = np.array(np.where(data_set[:,22] == 1)[0])
-    cat_C = np.array(np.where(data_set[:,22] == 2)[0])
-    cat_D = np.array(np.where(data_set[:,22] == 3)[0])
+    ind_class_0 = np.array(np.where(data_set[:,22] == 0)[0])
+    ind_class_1 = np.array(np.where(data_set[:,22] == 1)[0])
+    ind_class_2 = np.array(np.where(data_set[:,22] == 2)[0])
+    ind_class_3 = np.array(np.where(data_set[:,22] == 3)[0])
     
     # Return the results
-    return cat_A, cat_B, cat_C, cat_D
+    return ind_class_0, ind_class_1, ind_class_2, ind_class_3
 
+# -------------------------------------------------------------------------- #
 
+def classification(data_set):
+    # Put the data in one of the four classes in function of the indices
+    ind_class_0, ind_class_1, ind_class_2, ind_class_3 = indices_classification(data_set)
+    class_0 = data_set[ind_class_0]
+    class_1 = data_set[ind_class_1]
+    class_2 = data_set[ind_class_2]
+    class_3 = data_set[ind_class_3]
+    
+    # Return the results
+    return class_0, class_1, class_2, class_3
 
+# -------------------------------------------------------------------------- #
+
+def y_classification(y, data_set):
+    #return the y associated to each class
+    ind_class_0, ind_class_1, ind_class_2, ind_class_3 = indices_classification(data_set)
+    y_0 = y[ind_class_0]
+    y_1 = y[ind_class_1]
+    y_2 = y[ind_class_2]
+    y_3 = y[ind_class_3]
+    
+    return y_0, y_1, y_2, y_3
+
+# -------------------------------------------------------------------------- #
 
 def indices_outliers(feature):
     '''find outliers indices''' 
@@ -59,34 +133,87 @@ def indices_outliers(feature):
     # return the indices of the feature that contain outliers
     return ind_out     
 
+# -------------------------------------------------------------------------- #
 
 def treating_outliers(feature):
-    '''replace outliers' values by the mean'''
+    '''replace outliers' values by the median'''
     indices = np.arange(0, len(feature))
     ind_out = indices_outliers(feature)
     ind_in = np.delete(indices, ind_out)
     ind_in = ind_in.reshape(-1)
-    f_mean = feature[ind_in].mean()
-    feature[ind_out] = f_mean
-    # return feature with outliers' values replaced by the mean (without outliers' values)
+    f_median = np.percentile(feature[ind_in], 50)
+    feature[ind_out] = f_median
+    # return feature with outliers' values replaced by the median (without outliers' values)
     return feature
 
+# -------------------------------------------------------------------------- #
 
-
-def clean_train_set(train_set):
+'''def clean_train_set(train_set):
     # Loop over all the columns of the train set
     for i in range (train_set.shape[1]):
         train_set[:,i] = treating_outliers(train_set[:,i])
     
     # Return the cleaned train set
-    return train_set
+    return train_set'''
 
+# -------------------------------------------------------------------------- #
 
-
-def clean_test_set(test_set):
+'''def clean_test_set(test_set):
     # Loop over all the columns of the test set
     for i in range (test_set.shape[1]):
         test_set[:,i] = treating_outliers(test_set[:,i])
     
     # Return the cleaned test set
-    return test_set
+    return test_set'''
+
+# -------------------------------------------------------------------------- #
+
+''' comme clean_test_set et clean_train_test font la même chose mais juste sur des data set differents je me suis dit que c'était sûrement mieux de faire juste une fonction et on lui passe le train ou le test set en argument '''
+def clean_set(data_set):
+    # Loop over all the columns of the test set
+    for i in range (data_set.shape[1]):
+        data_set[:,i] = treating_outliers(data_set[:,i])
+    
+    # Return the cleaned data set
+    return data_set
+
+# -------------------------------------------------------------------------- #
+
+def tx_class_0(class_0):
+    '''delete features that shouldn't be taken into account for the model for class 0:
+    DER_deltaeta_jet_jet = feature 4
+    DER_mass_jet_jet = feature 5
+    DER_prodeta_jet_jet = feature 6
+    DER_pt_tot = feature 8
+    DER_sum_pt = feature 9
+    DER_lep_eta_centrality = feature 12
+    PRI_jet_leading_pt = feature 23
+    PRI_jet_leading_eta = feature 24
+    PRI_jet_leading_phi = feature 25
+    PRI_jet_subleading_pt = feature 26
+    PRI_jet_subleading_eta = feature 27
+    PRI_jet_subleading_phi = feature 28'''
+    
+    ind_features_to_delete = np.array([4, 5, 6, 8, 9, 12, 23, 24 , 25, 26, 27, 28]) 
+    class_0 = np.delete(class_0, ind_features_to_delete, axis = 1)
+    return class_0
+    
+# -------------------------------------------------------------------------- #
+
+def EDA(data_set):
+    ''' do all the EDA for a specific data set'''
+    # clean constant features
+    data_set = clean_constant_features(data_set)
+    
+    # clean outliers
+    data_set = clean_set(data_set)
+    
+    # standardization
+    data_set = standardize(data_set)
+    
+    # clean correlated features
+    data_set = clean_correlated_features(data_set)
+    
+    return data_set
+
+# -------------------------------------------------------------------------- #
--- Project 1/scripts/implementations.py
+++ Project 1/scripts/implementations.py
@@ -9,53 +9,48 @@ def compute_mse(y, tx, w):
     # Calculate the loss using the MSE formula
     
     # Definition of the parameters
-    MSE_loss = 0
+    mse = 0
     N = len(y)
-    error = y - tx.dot(w)
+    e = y - tx.dot(w)
     
     # Compute the loss MSE
-    MSE_loss = (1 / (2 * N)) * np.transpose(error).dot(error)
-    return MSE_loss
+    mse = (1 / (2 * N)) * e.T.dot(e)
+    return mse
 
 # -------------------------------------------------------------------------- #
 
 def compute_gradient_mse(y, tx, w):
     # Compute the gradient of the Mean Square Error
-    
-    # Definition of the parameters
-    N = len(y)
     e = y - tx.dot(w)
+    gradient = -tx.T.dot(e)/len(e)
     
-    # Compute directly the gradient
-    grad = (-1 / N) * np.transpose(tx).dot(e)
-    
-    return grad
+    return gradient
 
 # -------------------------------------------------------------------------- #
 
-def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+def least_squares_GD(y, tx, initial_w = 0, lambda_ = 0.7, gamma = 0.01, max_iters = 50):
     # Gradient descent algorithm
     
     # Definition of all the parameters
     loss = 0
-    weight = initial_w
+    w = initial_w
     
     # Loop for on the number of iterations
     for nb_iter in range(max_iters):
         
         # Compute the gradient
-        grad = compute_gradient_mse(y, tx, weight)
+        grad = compute_gradient_mse(y, tx, w)
         
         # Compute the loss (MSE)
-        loss = compute_mse(y, tx, weight)
+        loss = compute_mse(y, tx, w)
         
         # Update the weight parameters
-        weight = weight - (gamma * grad)
-    return loss, weight
+        w = w - (gamma * grad)
+    return loss, w
 
 # -------------------------------------------------------------------------- #
 
-def least_squares(y, tx):
+def least_squares(y, tx, initial_w = 0, lambda_ = 0.7, gamma = 0.01, max_iters = 50):
     """calculate the least squares solution."""
     a = tx.T.dot(tx)
     b = tx.T.dot(y)
@@ -66,7 +61,7 @@ def least_squares(y, tx):
 
 # -------------------------------------------------------------------------- #
 
-def ridge_regression(y, tx, lambda_):
+def ridge_regression(y, tx, initial_w = 0, lambda_ = 0.7, gamma = 0.01, max_iters = 50):
     """implement ridge regression."""
     a = tx.T.dot(tx) + 2*tx.shape[0]*lambda_*np.identity(tx.shape[1])
     b = tx.T.dot(y)
@@ -80,10 +75,11 @@ def compute_stoch_gradient(y, tx, w):
     """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
     e = y - tx.dot(w)
     
-    grad = -1/len(tx) * np.transpose(tx).dot(e)
+    gradient = -1/len(tx) * tx.T.dot(e)
     
-    return grad
+    return gradient
 
+# -------------------------------------------------------------------------- #
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     """
@@ -112,7 +108,7 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
 
 # -------------------------------------------------------------------------- #
 
-def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, batch_size = 10):
+def stochastic_gradient_descent(y, tx, initial_w = 0, lambda_ = 0.7, gamma = 0.01, max_iters = 50, batch_size = 10):
     """Stochastic gradient descent algorithm."""
     ws = [initial_w]
     losses = []
@@ -128,8 +124,9 @@ def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, batch_size =
             # store w and loss
             ws.append(w)
             losses.append(loss)
-            print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-                  bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+            '''uncomment if you want to print the losses for each steps'''
+            #print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+            #      bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
             
     return loss, w
 
@@ -138,26 +135,75 @@ def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, batch_size =
 def sigmoid(t):
     return 1 / (1 + np.exp(-t))
 
+# -------------------------------------------------------------------------- #
+
 def compute_loss_lr(y, tx, w):
     t = tx.dot(w)
     sigma = sigmoid(t)
     return - (y.T.dot(np.log(sigma)) + (1 - y).T.dot(np.log(1 - sigma)))
+
+# -------------------------------------------------------------------------- #
 
 def compute_gradient(y, tx, w):
     sigma = sigmoid(tx.dot(w))
     grad = np.transpose(tx).dot(sigma - y)
     return grad
 
-def logistic_regression_GD(y, tx, initial_w, gamma, max_iter):
-    # INitialization
+# -------------------------------------------------------------------------- #
+
+def logistic_regression_GD(y, tx, initial_w = 0, lambda_ = 0.7, gamma = 0.01, max_iters = 50):
+    # Initialization
     w = initial_w
     loss = 0
 
     # Loop for on the number of iterations
-    for nb_iter in range(max_iter):
+    for nb_iter in range(max_iters):
         loss = compute_loss_lr(y, tx, w)
         grad = compute_gradient(y, tx, w)
         w = w - gamma * grad
     
     # Return the results
     return loss, w
+
+# -------------------------------------------------------------------------- #
+
+def build_k_indices(y, k_fold, seed):
+    """build k indices for k-fold."""
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval]
+                 for k in range(k_fold)]
+    return np.array(k_indices)
+
+# -------------------------------------------------------------------------- #
+
+def cross_validation(y, x, k_indices, k, method, initial_w, lambda_ = 0.7, gamma = 0.01, max_iters = 50):
+    """return the loss of the method"""
+    loss_tr = [] #to save the training loss for each training set
+    loss_te = [] #to save the testint loss for each test set
+
+    # get k'th subgroup in test, others in train: 
+    te_idx = k_indices[k] # takes the indices of the data that corresponds to the k'th subgroup
+    tr_idx = k_indices[~(np.arange(k_indices.shape[0]) == k)] # select the subgroups that are not 
+                                                              # the test one and put them in train
+                                                              # np.arange is for creating an array 
+                                                              # containing the indices of the data 
+                                                              # that are for the train
+    tr_idx = tr_idx.reshape(-1) # put everything in a list
+    x_tr = x[tr_idx]
+    x_te = x[te_idx]
+    y_tr = y[tr_idx]
+    y_te = y[te_idx]
+        
+    # ridge regression:
+    loss_tr_i, w = method(y_tr, x_tr, initial_w, lambda_, gamma, max_iters)
+    
+    # calculate the loss for train and test data:       
+    loss_tr.append(loss_tr_i)
+    loss_te.append(compute_mse(y_te, x_te, w))
+
+    return loss_tr, loss_te
+
+# -------------------------------------------------------------------------- #

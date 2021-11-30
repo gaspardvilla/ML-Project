bea1a00e1b03f3bfe7275a64112834ced21966ec
GILLYBOEUF <marie-alix.gillyboeuf@epfl.ch>
2021-11-30 19:46:41 +0100

    lasso, PCA, recursive. Attention la librairie yellowbrick est a ajouter!!!

--- Project 2/Snowfalkes/helpers.py
+++ Project 2/Snowfalkes/helpers.py
@@ -1,6 +1,7 @@
 # Import the used libraries
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 
 
@@ -53,7 +54,7 @@ class MASCDB_classes:
         return class_cam
 
 
-    def get_classified_data_cam(self, classifier, cam, cam_data):
+    def get_sub_data_cam(self, classifier, cam, cam_data):
         # Get the classifier cam
         class_cam = self.get_class_cam(classifier, cam)
 
@@ -70,22 +71,22 @@ class MASCDB_classes:
         # Get the sub classes for this cam that are in cam_features
         sub_cam_classes = class_cam[class_cam['flake_id'].isin(cam_data['flake_id'])]
 
-        # Return the results
+        # Return the classes for a specific camera
         return sub_cam_classes
 
     def get_classified_data(self, classifier, data_set):
         # Create the input
         # Add for each cam
         # cam0
-        classified_data = self.get_classified_data_cam(classifier, 0, data_set.cam0)
+        classified_data = self.get_sub_data_cam(classifier, 0, data_set.cam0)
         
         #cam1
-        classified_data = pd.concat([classified_data, self.get_classified_data_cam(classifier, 1, data_set.cam1)])
+        classified_data = pd.concat([classified_data, self.get_sub_data_cam(classifier, 1, data_set.cam1)])
         
         #cam2
-        classified_data = pd.concat([classified_data, self.get_classified_data_cam(classifier, 2, data_set.cam2)])
+        classified_data = pd.concat([classified_data, self.get_sub_data_cam(classifier, 2, data_set.cam2)])
 
-        # Return the concatenated data frame
+        # Return the concatenated data frame that contains all the data point to consider
         return classified_data
 
     
@@ -99,6 +100,7 @@ class MASCDB_classes:
         # Append the classes that are in cam 2
         classes = pd.concat([classes, self.get_sub_classes_cam(classifier, 2, data.cam2)])
        
+        # Return all the labels 
         return classes
 
 
@@ -120,3 +122,75 @@ def numpy_helpers(df, cols):
 
 def cross_validation_method():
     return None
+
+def features_selection (X, y, method, param, plot = False):
+    if method == "lasso":
+        from sklearn.feature_selection import SelectFromModel
+        from sklearn.linear_model import Lasso
+        # define and fit the method
+        lasso = Lasso(alpha = param).fit(X, y)
+        model = SelectFromModel(lasso, prefit = True)
+        if plot == True:
+            importance = np.abs(lasso.coef_)
+            feature_names = np.array(X.columns)
+            plt.bar(height=importance, x=feature_names)
+            plt.title("Feature importances via coefficients")
+            plt.show()
+        return model.transform(X)
+
+    elif method == "lassoCV":
+        from sklearn.feature_selection import SelectFromModel
+        from sklearn.linear_model import LassoCV
+        # define and fit the method
+        lassoCV = LassoCV(cv = param).fit(X, y)
+        model = SelectFromModel(lassoCV, prefit = True)
+        if plot == True:
+            print('TODO')
+        # transform the data
+        return model.transform(X)
+
+    elif method == "PCA":
+        from sklearn.decomposition import PCA
+        # define the method
+        model = PCA(n_components = param)
+        # transform the data
+        components = model.fit_transform(X)
+        if plot == True:
+            pca = PCA()
+            pca.fit(X)
+            plt.plot(np.cumsum(pca.explained_variance_ratio_))
+            plt.xlabel('number of components')
+            plt.ylabel('cumulative explained variance')
+        return components
+
+    elif method == "recursive":
+        # define an estimator
+        from sklearn.svm import SVR
+        estimator = SVR(kernel="linear")
+        # define and fit the method
+        from sklearn.feature_selection import RFE
+        model = RFE(estimator, n_features_to_select=param)
+        if plot == True:
+            print('TODO')
+        # transform the data
+        return model.fit_transform(X, y)
+
+    elif method == "recursiveCV":
+        # define an estimator
+        from sklearn.svm import SVR
+        estimator = SVR(kernel="linear")
+        # define and fit the method
+        from sklearn.feature_selection import RFECV
+        model = RFECV(estimator, cv=param).fit(X, y)
+        if plot == True:
+            from sklearn.model_selection import StratifiedKFold
+            from yellowbrick.model_selection import RFECV
+            cv = StratifiedKFold(param)
+            visualizer = RFECV(estimator, cv=cv)
+            visualizer.fit(X, y)        # Fit the data to the visualizer
+            visualizer.show() 
+        # transform the data
+        return model.transform(X, y)
+        
+    else:
+        raise ValueError("Wrong method, it should be either: 'lasso', 'lassoCV', 'PCA', 'recursive' or 'recursiveCV'.")

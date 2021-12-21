8b17c0d5d477e137455d12f6820eae5d9df87e74
Baptiste <baptiste.hernette@epfl.ch>
2021-12-21 09:39:07 +0100

    mlp

--- Project 2/Snowfalkes/models.py
+++ Project 2/Snowfalkes/models.py
@@ -192,6 +192,7 @@ def get_model_MLP():
     param = {"activation" : ['tanh', 'relu'],
             "solver": ['sgd', 'adam'],
             "alpha": np.logspace(0,-4,5),
+            "learning_rate_init": np.logspace(-1,-5,10),
             "learning_rate": ['constant', 'adaptive']}
 			  
     return model, param
